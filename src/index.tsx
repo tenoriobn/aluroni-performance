539ab7b5f3c644d3072d7d36a882cacf4bc5613a
Bruno Tenório <tenoriobn@gmail.com>
2023-12-21 10:58:53 -0300

    utilizando React.memo para evitar renderizações desnecessárias

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import './index.css';
+import Router from './routes.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div> Aluroni </div>
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -0,0 +1,30 @@
+import React, { memo } from 'react';
+import styles from './Item.module.scss'; 
+import { Prato } from 'types/Prato';
+import TagsPrato from 'components/TagsPrato';
+import { useNavigate } from 'react-router-dom';
+
+function Item(props: Prato) {
+  const { id, title, description, photo } = props;
+
+  const navigate = useNavigate();
+
+  id === 1 && console.log(title, props);
+
+  return (
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
+      <div className={styles.item__imagem}>
+        <img src={photo} alt={title} />
+      </div>
+      <div className={styles.item__descricao}>
+        <div className={styles.item__titulo}>
+          <h2> {title} </h2>
+          <p> {description} </p>
+        </div>
+        <TagsPrato {...props} />
+      </div>
+    </div>
+  );
+}
+
+export default memo(Item);
