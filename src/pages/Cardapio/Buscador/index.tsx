13d0f8ce8f1633279f530d55ea29acb9d997b251
Bruno Tenório <tenoriobn@gmail.com>
2023-12-25 10:18:21 -0300

    utilizando useMemo no Buscador

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import styles from './Buscador.module.scss';
 import { CgSearch } from 'react-icons/cg';
 
@@ -7,7 +7,9 @@ interface Props {
   setBusca: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Buscador({ busca, setBusca }: Props) {
+function Buscador({ busca, setBusca }: Props) {
+  const elemento = useMemo(() => <CgSearch size={20} color="#4C4D5E" />, []);
+
   return (
     <div className={styles.buscador}>
       <input
@@ -15,7 +17,9 @@ export default function Buscador({ busca, setBusca }: Props) {
         onChange={(evento) => setBusca(evento.target.value)}
         placeholder="Buscar"
       />
-      <CgSearch size={20} color="#4C4D5E" />
+      {elemento}
     </div>
   );
 }
+
+export default memo(Buscador);
